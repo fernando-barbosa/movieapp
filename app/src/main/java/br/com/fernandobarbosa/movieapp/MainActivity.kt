cf5933e7217b0e74aef85a08e4cdb897ceed8e6c
fernandobarbosa <fernandobsjunior@gmail.com>
2021-12-27 17:04:43 -0300

    Add click event to the card

--- app/src/main/java/br/com/fernandobarbosa/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/fernandobarbosa/movieapp/MainActivity.kt
@@ -1,18 +1,23 @@
 package br.com.fernandobarbosa.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fernandobarbosa.movieapp.ui.theme.MovieAppTheme
@@ -48,35 +53,50 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MainContent(movieList: List<String> = listOf(
-    "Star Wars", "Tron", "Matrix", "GhostBusters", "Lord of the Rings")) {
+    "Star Wars", "Tron", "Matrix", "GhostBusters", "Lord of the Rings", "The Social Network", "Karate Kid")) {
     
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn {
             items(items = movieList) {
-                MovieRow(movie = it)
+                MovieRow(movie = it) { movie ->
+                    Log.d("TAG", "Movie: $movie")
+                }
             }
         }
     }
 }
 
 @Composable
-fun MovieRow(movie: String) {
+fun MovieRow(movie: String, onClickItem: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp),
+            .height(130.dp)
+            .clickable { onClickItem(movie) },
         shape = RoundedCornerShape(
             corner = CornerSize(16.dp)),
-        elevation = 6.dp) {
+        elevation = 6.dp,
+    ) {
 
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
+            }
+            Text(text = movie)
         }
-        Text(text = movie)
     }
 }
 
