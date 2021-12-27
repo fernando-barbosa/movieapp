194bb94225d6e81096e9a8876c87042f6abec322
fernandobarbosa <fernandobsjunior@gmail.com>
2021-12-27 14:33:55 -0300

    Add Movie card

--- app/src/main/java/br/com/fernandobarbosa/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/fernandobarbosa/movieapp/MainActivity.kt
@@ -3,8 +3,15 @@ package br.com.fernandobarbosa.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,9 +47,36 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello")
+fun MainContent(movieList: List<String> = listOf(
+    "Star Wars", "Tron", "Matrix", "GhostBusters", "Lord of the Rings")) {
+    
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(
+            corner = CornerSize(16.dp)),
+        elevation = 6.dp) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+
+        }
+        Text(text = movie)
     }
 }
 
