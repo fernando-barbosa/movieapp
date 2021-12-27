cf8ee60b44a4d5ad72a395d915322cf777a3f935
fernandobarbosa <fernandobsjunior@gmail.com>
2021-12-27 14:03:11 -0300

    Add TopAppBar do Main Activity usin Scaffold

--- app/src/main/java/br/com/fernandobarbosa/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/fernandobarbosa/movieapp/MainActivity.kt
@@ -3,36 +3,53 @@ package br.com.fernandobarbosa.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.fernandobarbosa.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    backgroundColor = Color.Magenta,
+                    elevation = 5.dp                
+                ) {
+                    Text(text = "Movies")
+                }
+            }
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "Hello")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
