f68516b1a88a3c76196d39bac0541fc58c9eff35
fernandobarbosa <fernandobsjunior@gmail.com>
2021-12-30 11:46:12 -0300

    Add image to MovieRow widget and expand arrow

--- app/src/main/java/br/com/fernandobarbosa/movieapp/widgets/Widgets.kt
+++ app/src/main/java/br/com/fernandobarbosa/movieapp/widgets/Widgets.kt
@@ -1,25 +1,38 @@
 package br.com.fernandobarbosa.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fernandobarbosa.movieapp.model.Movie
 import br.com.fernandobarbosa.movieapp.model.getMovies
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 
+@ExperimentalAnimationApi
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0],
              onClickItem: (String) -> Unit = {}) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -40,10 +53,18 @@ fun MovieRow(movie: Movie = getMovies()[0],
                 modifier = Modifier
                     .padding(12.dp)
                     .size(100.dp),
-                shape = RectangleShape,
-                elevation = 4.dp
+                shape = RectangleShape
             ) {
-
+                Image(
+                    painter = rememberImagePainter(
+                        data = movie.images[0],
+                        builder = {
+                            crossfade(true)
+                            transformations(CircleCropTransformation())
+                        }
+                    ),
+                    contentDescription = "Movie poster"
+                )
             }
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(
@@ -57,6 +78,25 @@ fun MovieRow(movie: Movie = getMovies()[0],
                 Text(
                     text = "Release: ${movie.year}",
                     style = MaterialTheme.typography.caption
+                )
+
+                AnimatedVisibility(visible = expanded) {
+                    Column(
+
+                    ) {
+                        Text(text = "Hello there")
+                    }
+                }
+
+                Icon(
+                    imageVector =
+                        if (expanded) Icons.Default.KeyboardArrowUp
+                        else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Expand arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable { expanded = !expanded },
+                    tint = Color.DarkGray
                 )
             }
         }
